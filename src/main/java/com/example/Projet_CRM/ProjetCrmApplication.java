0b8d13fdeebab485d9ab3c00343e13eed55690b6
Eric <larremdy@labom2iformation.fr>
2024-02-02 10:20:37 +0100

    Premier commit

---
+++ src/main/java/com/example/Projet_CRM/ProjetCrmApplication.java
@@ -0,0 +1,13 @@
+package com.example.Projet_CRM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetCrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Projet_CRM/ProjetCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Projet_CRM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
